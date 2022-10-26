993cc853800a15136f9ad7245e1224cb14279da8
Ubuntu <ubuntu@ip-172-31-43-4.eu-west-1.compute.internal>
2022-10-26 16:53:04 +0000

    fix bugs + add filtering by length, missing values and resolution

--- utils/preprocess_sidechainnet.py
+++ utils/preprocess_sidechainnet.py
@@ -1,6 +1,8 @@
+from email.policy import default
 import editdistance
 from collections import defaultdict
 import numpy as np
+from tqdm import tqdm
 
 
 def get_keys_ids(data_scn, pdb_ids=None, dataset="train"):
@@ -22,7 +24,8 @@ def get_duplicate_weird_chains(data_scn, pdb_ids=None, dataset="train"):
 
     key_dict, pdb_ids = get_keys_ids(data_scn, pdb_ids, dataset)
     res = defaultdict(lambda: [])
-    for key in pdb_ids:
+    print('Searching for duplicates...')
+    for key in tqdm(pdb_ids):
         chains = key_dict[key]
         main_chains = [x.split('_')[-1].lower() for x in chains if len(x.split('_')[-1]) <= 2]
         weird_chains = [x for x in chains if len(x.split('_')[-1]) > 2]
@@ -39,7 +42,8 @@ def get_weird_chain_sequence_id(data_scn, pdb_ids=None, dataset="train"):
 
     key_dict, pdb_ids = get_keys_ids(data_scn, pdb_ids, dataset)
     sequence_ids = defaultdict(lambda: {})
-    for key in pdb_ids:
+    print('Calculating sequence similarities...')
+    for key in tqdm(pdb_ids):
         chains = key_dict[key]
         main_chains = [x for x in chains if len(x.split('_')[-1]) <= 2]
         weird_chains = [x for x in chains if len(x.split('_')[-1]) > 2]
@@ -64,16 +68,17 @@ def get_weird_chain_sequence_id(data_scn, pdb_ids=None, dataset="train"):
             sequence_ids[key][weird_chain_id] = {"similarity": max(metrics), "chains": chain_list}
     return sequence_ids
     
-def get_identical_sequence_weird_chains(data_scn, pdb_ids=None, thr=0.9, dataset="train"):
+def get_identical_sequence_weird_chains(data_scn, pdb_ids=None, similarity_thr=0.9, dataset="train"):
     """
     Get a dictionary of recombined weird chain lists that have a high similarity to a main chain
     """
 
     sequence_ids = get_weird_chain_sequence_id(data_scn, pdb_ids, dataset)
     res = defaultdict(lambda: {})
-    for key, id_dict in sequence_ids.items():
+    print('Finding chains to combine...')
+    for key, id_dict in tqdm(sequence_ids.items()):
         for chain_id, value in id_dict.items():
-            if value["similarity"] > thr:
+            if value["similarity"] > similarity_thr:
                 res[key][chain_id] = value["chains"]
     return res
 
@@ -84,14 +89,16 @@ def get_minus_weird_chains(data_scn, pdb_ids=None, dataset="train"):
 
     key_dict, pdb_ids = get_keys_ids(data_scn, pdb_ids, dataset)
     res = {}
-    for key in pdb_ids:
+    print('Searching for shortened chains...')
+    for key in tqdm(pdb_ids):
         main_chains = [x for x in key_dict[key] if len(x.split('_')[-1]) <= 2]
         main_chain_ids = [x.split('_')[-1].lower() for x in main_chains]
-        minus_chains = [x for x in key_dict[key] if len(x.split("_")[-1]) > 2]
-        res[key] = {
-            "repeated": [x for x in minus_chains if x[10].lower() in main_chain_ids],
-            "unique": [x for x in minus_chains if x[10].lower() not in main_chain_ids]
-        }
+        minus_chains = [x for x in key_dict[key] if x.split("_")[-1] == "-"]
+        if len(minus_chains) > 0:
+            res[key] = {
+                "repeated": [x for x in minus_chains if x[10].lower() in main_chain_ids],
+                "unique": [x for x in minus_chains if x[10].lower() not in main_chain_ids]
+            }
     return res
 
 def shorten_key_dict(key_dict, other_dict):
@@ -119,20 +126,52 @@ def filter_main_chain(key_dict):
             key_dict.pop(key)
     return key_dict
 
-def filter_weird_chains(data_scn, dataset, thr=0.9):
+def filter_chain_pars(
+        data_scn, 
+        dataset="train", 
+        resolution_thr=3.5,
+        missing_thr=0.9,
+        length_thr=30,
+    ):
+    key_dict, _ = get_keys_ids(data_scn, dataset=dataset)
+    result = defaultdict(lambda: [])
+    print('Filtering by resolution, length and missing values...')
+    for i, res in tqdm(enumerate(data_scn[dataset]["res"]), total=len(data_scn[dataset]["res"])):
+        key = data_scn[dataset]["ids"][i].split('_')[0]
+        if res is None or res > resolution_thr:
+            result[key] = key_dict[data_scn[dataset]["ids"][i].split('_')[0]]
+            continue
+        nm = (np.array(list(data_scn[dataset]["msk"][i])) == "+").sum()
+        total_len = len(data_scn[dataset]["msk"][i])
+        if nm < length_thr:
+            result[key].append(data_scn[dataset]["ids"][i])
+        elif nm / total_len < missing_thr:
+            result[key] = key_dict[data_scn[dataset]["ids"][i].split('_')[0]]
+    return result
+
+def filter_weird_chains(data_scn, dataset, similarity_thr=0.9, resolution_thr=3.5, length_thr=30, missing_thr=0.9):
     """
     Generate lists of chains to remove, combine and keep (in this order)
     """
 
     key_dict, _ = get_keys_ids(data_scn, dataset)
-    minus_chain_dict = get_minus_weird_chains(data_scn, dataset)
+    minus_chain_dict = get_minus_weird_chains(data_scn, dataset=dataset)
     key_dict = shorten_key_dict(key_dict, minus_chain_dict)
     dup_chain_dict = get_duplicate_weird_chains(data_scn, pdb_ids=list(key_dict.keys()), dataset=dataset)
     key_dict = shorten_key_dict(key_dict, dup_chain_dict)
-    id_seq_chain_dict = get_identical_sequence_weird_chains(data_scn, pdb_ids=list(key_dict.keys()), thr=thr, dataset=dataset)
+    id_seq_chain_dict = get_identical_sequence_weird_chains(data_scn, pdb_ids=list(key_dict.keys()), similarity_thr=similarity_thr, dataset=dataset)
     key_dict = shorten_key_dict(key_dict, id_seq_chain_dict)
+    res_dict = filter_chain_pars(
+        data_scn,
+        dataset, 
+        resolution_thr=resolution_thr,
+        length_thr=length_thr,
+        missing_thr=missing_thr,
+    )
     to_remove = []
     for value in key_dict.values():
+        to_remove += value
+    for value in res_dict.values():
         to_remove += value
     for value in minus_chain_dict.values():
         to_remove += value["repeated"]
@@ -159,41 +198,89 @@ def combine_data(main_data_scn, add_data_scn, dataset="train"):
         for chain in add_key_dict[key]:
             if chain not in value:
                 to_add.append(chain)
+    print(f'Adding {len(to_add)} chains...')
     for i, key in enumerate(add_data_scn[dataset]["ids"]):
         if key in to_add:
             for x in main_data_scn[dataset]:
                 main_data_scn[dataset][x].append(add_data_scn[dataset][x][i])
     return main_data_scn
 
-def preprocess(data_scn, dataset="train", thr=0.9):
+def preprocess(data_scn, dataset="train", similarity_thr=0.9, resolution_thr=3.5, length_thr=30, missing_thr=0.1):
     """
     Filter weird chains and combine chains with the same PDB ID into single entries
     """
     
-    _, to_combine, to_keep = filter_weird_chains(data_scn, dataset=dataset, thr=thr)
+    to_remove, to_combine, to_keep = filter_weird_chains(
+        data_scn, 
+        dataset=dataset, 
+        similarity_thr=similarity_thr,
+        resolution_thr=resolution_thr,
+        length_thr=length_thr,
+        missing_thr=missing_thr,
+    )
+    print(f'Removing {len(to_remove)} chains...')
+    print(f'Recombining chains...')
     to_keep_pdbs = defaultdict(lambda: [])
     for key in to_keep:
         to_keep_pdbs[key.split('_')[0]].append(key)
     new_data = defaultdict(lambda: [])
-    for chain_list in to_combine:
+    for chain_list in tqdm(to_combine):
         chain_id = chain_list[0][10].upper()
         pdb_id = chain_list[0].split('_')[0]
         chain_indices = [data_scn[dataset]["ids"].index(x) for x in chain_list]
         new_chain = f"{pdb_id}_r_{chain_id}"
-        new_seq = sum([data_scn[dataset]["seq"][i] for i in chain_indices])
-        new_crd = np.concatenate([data_scn["seq"][i] for i in chain_indices])
+        new_seq = ''.join([data_scn[dataset]["seq"][i] for i in chain_indices])
+        new_msk = ''.join([data_scn[dataset]["msk"][i] for i in chain_indices])
+        new_crd = np.concatenate([data_scn[dataset]["crd"][i] for i in chain_indices])
         data_scn[dataset]["ids"].append(new_chain)
         data_scn[dataset]["seq"].append(new_seq)
         data_scn[dataset]["crd"].append(new_crd)
+        data_scn[dataset]["msk"].append(new_msk)
         to_keep_pdbs[pdb_id].append(new_chain)
-
-    for key in to_keep_pdbs:
+    print(f'Generating multi-chain entries...')
+    for key in tqdm(to_keep_pdbs):
         chain_list = to_keep_pdbs[key]
         chain_indices = [data_scn[dataset]["ids"].index(x) for x in sorted(chain_list)]
         new_seq = [data_scn[dataset]["seq"][i] for i in chain_indices]
-        new_crd = [data_scn["seq"][i] for i in chain_indices]
+        new_crd = [data_scn[dataset]["crd"][i] for i in chain_indices]
+        new_msk = [data_scn[dataset]["msk"][i] for i in chain_indices]
         new_data["ids"].append(key)
         new_data["scn"].append(sorted(chain_list))
         new_data["seq"].append(new_seq)
         new_data["crd"].append(new_crd)
+        new_data["msk"].append(new_msk)
     return new_data
+
+def cut_missing_ends(data):
+    """
+    Cut intervals at the end and start of sequences where structure information is missing
+    """
+
+    cut_start = 0
+    start_lens = []
+    cut_end = 0
+    end_lens = []
+    print('Cutting missing ends...')
+    for i in tqdm(range(len(data["ids"]))):
+        for j, mask in enumerate(data["msk"][i]):
+            k = 0
+            while mask[k] == "-":
+                k += 1
+            if k > 0:
+                cut_start += 1
+                start_lens.append(k)
+                data["msk"][i][j] = data["msk"][i][j][: k]
+                data["crd"][i][j] = data["crd"][i][j][: k * 14]
+                data["seq"][i][j] = data["seq"][i][j][: k]
+            k = len(data["msk"][i][j]) - 1
+            while mask[k] == "-":
+                k -= 1
+            k += 1
+            if k < len(data["msk"][i][j]) - 1:
+                cut_end += 1
+                end_lens.append(len(data["msk"][i][j]) - 1 - k)
+                data["msk"][i][j] = data["msk"][i][j][k:]
+                data["crd"][i][j] = data["crd"][i][j][k * 14:]
+                data["seq"][i][j] = data["seq"][i][j][k:]
+    print(f'Cut {cut_start} start intervals (mean length {np.array(start_lens).mean():.1f}) and {cut_end} end intervals (mean length {np.array(end_lens).mean():.1f})')
+    return data
