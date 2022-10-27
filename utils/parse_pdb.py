c834de7f77d2f2173d90bc375b4a0dc4f95079f6
Liza Kozlova <el.mi.kozlova@gmail.com>
2022-10-27 17:41:04 +0000

    start writing PDB processing

--- utils/parse_pdb.py
+++ utils/parse_pdb.py
@@ -1,5 +1,34 @@
 from typing import List, Dict
 
+
+side_chain = {
+    "CYS": ["CB", "SG"],
+    "ASP": ["CB", "CG", "OD1", "OD2"],
+    "SER": ["CB", "OG"],
+    "GLN": ["CB", "CG", "CD", "OE1", "NE2"],
+    "LYS": ["CB", "CG", "CD", "CE", "NZ"],
+    "ILE": ["CB", "CG1", "CG2", "CD1"],
+    "PRO": ["CB", "CG", "CD"],
+    "THR": ["CB", "OG1", "CG2"],
+    "PHE": ["CB", "CG", "CD1", "CD2", "CE1", "CE2", "CZ"],
+    "ASN": ["CB", "CG", "OD1", "ND2"],
+    "GLY": [],
+    "HIS": ["CB", "CG", "ND1", "CD2", "CE1", "NE2"],
+    "LEU": ["CB", "CG", "CD1", "CD2"],
+    "ARG": ["CB", "CG", "CD", "NE", "CZ", "NH1", "NH2"],
+    "TRP": ["CB", "CG", "CD1", "CD2", "NE1", "CE2", "CE3", "CZ2", "CZ3", "CH2"],
+    "ALA": ["CB"],
+    "VAL": ["CB", "CG1", "CG2"],
+    "GLU": ["CB", "CG", "CD", "OE1", "OE2"],
+    "TYR": ["CB", "CG", "CD1", "CD2", "CE1", "CE2", "CZ", "OH"],
+    "MET": ["CB", "CG", "SD", "CE"],
+}
+
+d3to1 = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+ 'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N', 
+ 'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W', 
+ 'ALA': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
+
 def open_pdb(file_path: str, thr_resolution: float = 3.5) -> Dict:
     """
     Read a PDB file and parse it into a dictionary if it meets criteria
@@ -25,6 +54,21 @@ def open_pdb(file_path: str, thr_resolution: float = 3.5) -> Dict:
         the parsed dictionary or `None`, if the criteria are not met
 
     """
+    output = {}
+    if not crd["residue_name"].isin(d3to1.keys()).all():
+        print('error')
+    for chain in crd["chain_id"].unique():
+        chain_crd = crd[crd["chain_id"] == chain]
+        indices = np.unique(chain_crd["residue_number"], return_index=True)[1]
+        pdb_seq = "".join([d3to1[x] for x in chain_crd.loc[indices]["residue_name"]])
+        output["seq"] = pairwise2.align.globalms(pdb_seq, fasta[chain], 2, -1, -.5, -.1)[0][0]
+        output["msk"] = (np.array(list(output["seq"])) != "-").astype(int)
+        l = sum(output["msk"])
+        if l < min_length or l > max_length:
+            print('error')
+        output["crd_bb"] = np.zeros((l, 4, 3))
+        output["crd_sc"] = np.zeros((l, 10, 3))
+        bb_names = ["N", "C", "CA", "O"]
 
 def align_pdb(pdb_dict: Dict, min_length: int = 30, max_length: int = None, max_missing: float = 0.1) -> Dict:
     """
