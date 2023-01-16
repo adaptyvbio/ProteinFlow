6be9fd9684193470eee35e334474721b72ead68e
Liza <el.mi.kozlova@gmail.com>
2023-01-16 14:32:55 +0000

    add the download script

--- bestprot/scripts/download_data.py
+++ bestprot/scripts/download_data.py
@@ -1,2 +1,20 @@
+import click
+from bestprot import download_data
 
 
+@click.option(
+    "--tag",
+    help="The name of the dataset",
+)
+@click.option(
+    "--local_datasets_folder",
+    default="./data",
+    help="The folder where bestprot datasets, temporary files and logs will be stored"
+)
+@click.command()
+def main(**kwargs):
+    download_data(**kwargs)
+
+
+if __name__ == "__main__":
+    main()
