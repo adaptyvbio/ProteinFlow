1bfdb1de85ca99f575da948de8a0e66b544359f7
Liza <el.mi.kozlova@gmail.com>
2023-01-19 16:04:31 +0000

    minor changes in setup

--- setup.py
+++ setup.py
@@ -1,13 +1,6 @@
-import os
 from setuptools import setup
 from setuptools import find_packages
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='bestprot',
@@ -33,28 +26,10 @@ setup(
     description = ("A library for running the BestProt protein data processing pipeline."),
     entry_points={
         "console_scripts": [
-            "generate_bestprot = bestprot._scripts.generate_data:main",
-            "download_bestprot = bestprot._scripts.download_data:main",
-            "split_bestprot = bestprot._scripts.split_data:main"
+            "generate_bestprot = bestprot.scripts.generate_data:main",
+            "download_bestprot = bestprot.scripts.download_data:main",
+            "split_bestprot = bestprot.scripts.split_data:main"
         ]
     },
     python_requires=">=3.8,<3.10",
 )
-
-# setup(
-#     name = "bestprot",
-#     version = "0.0.1",
-#     author = "Adaptyv Biosystems",
-#     author_email = "contact@adaptyvbio.com",
-#     description = ("A library for using the BestProt protein data processing pipeline."),
-#     license = "BSD",
-#     keywords = "protein dataset",
-#     # url = "http://packages.python.org/an_example_pypi_project",
-#     packages=find_packages(),
-#     long_description=read('README.md'),
-#     classifiers=[
-#         "Development Status :: 3 - Alpha",
-#         "Topic :: Utilities",
-#         "License :: OSI Approved :: BSD License",
-#     ],
-# )
