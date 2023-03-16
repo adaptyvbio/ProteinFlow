bed0f52b6cacc8b6903a9e32f8782fab9adde955
Liza Kozlova <69125261+elkoz@users.noreply.github.com>
2023-03-16 10:26:48 +0100

    Update __init__.py

--- proteinflow/__init__.py
+++ proteinflow/__init__.py
@@ -16,6 +16,10 @@ In addition, `proteinflow` depends on the `rcsbsearch` package and the latest re
 python -m pip install "rcsbsearch @ git+https://github.com/sbliven/rcsbsearch@dbdfe3880cc88b0ce57163987db613d579400c8e"
 ```
 
+Note that you do not need to install `mmseqs` or `rcsbsearch` if you are not planning to generate a new dataset.
+
+Finally, you can use our [docker image](https://hub.docker.com/r/adaptyvbio/proteinflow/tags) as an alternative.
+
 ## Usage
 ### Downloading pre-computed datasets
 We have already run the pipeline with a consensus set of parameters and saved the results at a server. You can download the resulting dataset with `proteinflow`. Check the output of `proteinflow check_tags` for a list of available tags.
