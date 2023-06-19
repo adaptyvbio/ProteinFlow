d3a4d5e00c16fc92fe307af2f691d9a15ecf9cd8
Liza Kozlova <liza@adaptyvbio.com>
2023-06-19 15:08:07 +0000

    add module docstring

---
+++ proteinflow/metrics/__init__.py
@@ -0,0 +1,26 @@
+"""Metrics for evaluating prediction quality."""
+
+import blosum as bl
+
+
+def blosum62(seq_before, seq_after):
+    """Calculate the BLOSUM62 score between two sequences.
+
+    Parameters
+    ----------
+    seq_before : str
+        The sequence before the mutation.
+    seq_after : str
+        The sequence after the mutation.
+
+    Returns
+    -------
+    score : int
+        The BLOSUM62 score between the two sequences.
+
+    """
+    matrix = bl.BLOSUM(62)
+    score = 0
+    for x_before, x_after in zip(seq_before, seq_after):
+        score += matrix[x_before][x_after]
+    return score
