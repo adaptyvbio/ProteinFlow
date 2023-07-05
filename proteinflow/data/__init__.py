c0256f68ce9bb9bfdf044b05ada81b1902f56275
Liza Kozlova <liza@adaptyvbio.com>
2023-07-05 19:33:09 +0000

    wip: add PDBEntry and start sabdab

--- proteinflow/data/__init__.py
+++ proteinflow/data/__init__.py
@@ -1,6 +1,13 @@
+import os
 import pickle
+import urllib
+import warnings
 
 import numpy as np
+import requests
+from Bio import pairwise2
+from biopandas.pdb import PandasPdb
+from methodtools import lru_cache
 
 from proteinflow.constants import (
     _PMAP,
@@ -10,10 +17,97 @@ from proteinflow.constants import (
     BACKBONE_ORDER,
     CDR_ALPHABET,
     CDR_REVERSE,
+    CDR_VALUES,
     MAIN_ATOM_DICT,
     SIDECHAIN_ORDER,
 )
-from proteinflow.data.utils import _annotate_sse, _dihedral_angle
+from proteinflow.data.utils import *
+
+
+def _download_file(url, local_path):
+    """Download a file from a URL to a local path"""
+    response = requests.get(url)
+    open(local_path, "wb").write(response.content)
+
+
+def download_pdb(pdb_id, local_folder=".", sabdab=False):
+    """
+    Download a PDB file from the RCSB PDB database.
+
+    Parameters
+    ----------
+    pdb_id : str
+        PDB ID of the protein to download, can include a biounit index separated
+        by a dash (e.g. "1a0a", "1a0a-1")
+    local_folder : str, default "."
+        Folder to save the downloaded file to
+    sabdab : bool, default False
+        If True, download from the SAbDab database (Chothia style) instead of RCSB PDB
+
+    Returns
+    -------
+    local_path : str
+        Path to the downloaded file
+
+    """
+    if sabdab:
+        try:
+            url = f"https://opig.stats.ox.ac.uk/webapps/sabdab-sabpred/sabdab/pdb/{pdb_id}/?scheme=chothia"
+            local_path = os.path.join(local_folder, f"{pdb_id}.pdb")
+            _download_file(url, local_path)
+            return local_path
+        except:
+            raise RuntimeError(f"Could not download {pdb_id}")
+    if "-" in pdb_id:
+        pdb_id, biounit = pdb_id.split("-")
+        filenames = {
+            "cif": f"{pdb_id}-assembly{biounit}.cif.gz",
+            "pdb": f"{pdb_id}.pdb{biounit}.gz",
+        }
+        local_name = f"{pdb_id}-{biounit}"
+    else:
+        filenames = {
+            "cif": f"{pdb_id}.cif.gz",
+            "pdb": f"{pdb_id}.pdb.gz",
+        }
+        local_name = pdb_id
+    for t in filenames:
+        local_path = os.path.join(local_folder, local_name + f".{t}.gz")
+        try:
+            url = f"https://files.rcsb.org/download/{filenames[t]}"
+            _download_file(url, local_path)
+            return local_path
+        except BaseException:
+            pass
+    raise RuntimeError(f"Could not download {pdb_id}")
+
+
+def download_fasta(pdb_id, local_folder="."):
+    """
+    Download a FASTA file from the RCSB PDB database.
+
+    Parameters
+    ----------
+    pdb_id : str
+        PDB ID of the protein to download
+    local_folder : str, default "."
+        Folder to save the downloaded file to
+
+    Returns
+    -------
+    local_path : str
+        Path to the downloaded file
+
+    """
+    if "-" in pdb_id:
+        pdb_id = pdb_id.split("-")[0]
+    downloadurl = "https://www.rcsb.org/fasta/entry/"
+    pdbfn = pdb_id + "/download"
+    local_path = os.path.join(local_folder, f"{pdb_id.lower()}.fasta")
+
+    url = downloadurl + pdbfn
+    urllib.request.urlretrieve(url, local_path)
+    return local_path
 
 
 class ProteinEntry:
@@ -389,3 +483,369 @@ class ProteinEntry:
                 orientation[S_mask] = np.random.rand(*orientation[S_mask].shape)
         orientation /= np.expand_dims(np.linalg.norm(orientation, axis=-1), -1) + 1e-7
         return orientation
+
+
+class PDBEntry:
+    def __init__(self, pdb_path, fasta_path):
+        """A class for parsing PDB files
+
+        Parameters
+        ----------
+        pdb_path : str
+            Path to the PDB file
+        fasta_path : str
+            Path to the FASTA file
+
+        """
+        self.pdb_path = pdb_path
+        self.fasta_path = fasta_path
+        self.pdb_id = os.path.basename(self.fasta_path).split(".")[0]
+        self.crd_df, self.seq_df, self.fasta_dict = self._parse()
+
+    @staticmethod
+    def from_id(pdb_id, local_folder="."):
+        """Initialize a `PDBEntry` object from a PDB Id
+
+        Downloads the PDB and FASTA files to the local folder.
+
+        Parameters
+        ----------
+        pdb_id : str
+            PDB Id of the protein
+        local_folder : str, default '.'
+            Folder where the downloaded files will be stored
+
+        Returns
+        -------
+        entry : PDBEntry
+            A `PDBEntry` object
+
+        """
+        pdb_path = download_pdb(pdb_id, local_folder)
+        fasta_path = download_fasta(pdb_id, local_folder)
+        return PDBEntry(pdb_path=pdb_path, fasta_path=fasta_path)
+
+    @staticmethod
+    def parse_fasta(fasta_path):
+        """Read a fasta file
+
+        Parameters
+        ----------
+        fasta_path : str
+            Path to the fasta file
+
+        Returns
+        -------
+        out_dict : dict
+            A dictionary containing all the (author) chains in a fasta file (keys)
+            and their corresponding sequence (values)
+
+        """
+        with open(fasta_path) as f:
+            lines = np.array(f.readlines())
+
+        indexes = np.array([k for k, l in enumerate(lines) if l[0] == ">"])
+        starts = indexes + 1
+        ends = list(indexes[1:]) + [len(lines)]
+        names = lines[indexes]
+        seqs = ["".join(lines[s:e]).replace("\n", "") for s, e in zip(starts, ends)]
+
+        out_dict = {}
+        for name, seq in zip(names, seqs):
+            for chain in _retrieve_chain_names(name):
+                out_dict[chain] = seq
+
+        return out_dict
+
+    def _parse(self, chains=None):
+        cif = self.pdb_path.endswith("cif.gz")
+
+        # download fasta and check if it contains only proteins
+        try:
+            seqs_dict = self.parse_fasta(self.fasta_path)
+        except FileNotFoundError:
+            raise PDBError("Fasta file not found")
+
+        # load coordinates in a nice format
+        try:
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore")
+                if cif:
+                    p = CustomMmcif().read_mmcif(self.pdb_path).get_model(1)
+                else:
+                    p = PandasPdb().read_pdb(self.pdb_path).get_model(1)
+        except FileNotFoundError:
+            raise PDBError("PDB / mmCIF file downloaded but not found")
+        crd_df = p.df["ATOM"]
+        seq_df = p.amino3to1()
+
+        # retrieve sequences that are relevant for this PDB from the fasta file
+        if chains is None:
+            chains = p.df["ATOM"]["chain_id"].unique()
+        seqs_dict = {k.upper(): v for k, v in seqs_dict.items()}
+        if all([len(x) == 3 and len(set(list(x))) == 1 for x in seqs_dict.keys()]):
+            seqs_dict = {k[0]: v for k, v in seqs_dict.items()}
+
+        if not {x.split("-")[0].upper() for x in chains}.issubset(
+            set(list(seqs_dict.keys()))
+        ):
+            raise PDBError("Some chains in the PDB do not appear in the fasta file")
+
+        fasta_dict = {k: seqs_dict[k.split("-")[0].upper()] for k in chains}
+        return crd_df, seq_df, fasta_dict
+
+    def pdb_df(self, chain=None):
+        """Return the PDB dataframe
+
+        If `chain` is provided, only information for this chain is returned.
+
+        Parameters
+        ----------
+        chain : str, optional
+            Chain identifier
+
+        Returns
+        -------
+        df : pd.DataFrame
+            A `BioPandas` style dataframe containing the PDB information
+
+        """
+        if chain is None:
+            return self.crd_df
+        else:
+            return self.crd_df[self.crd_df["chain_id"] == chain]
+
+    def sequence_df(self, chain=None):
+        """Return the sequence dataframe
+
+        If `chain` is provided, only information for this chain is returned.
+
+        Parameters
+        ----------
+        chain : str, optional
+            Chain identifier
+
+        Returns
+        -------
+        df : pd.DataFrame
+            A dataframe containing the sequence and chain information
+            (analogous to the `BioPandas.pdb.PandasPdb.amino3to1` method output)
+
+        """
+        if chain is None:
+            return self.seq_df
+        else:
+            return self.seq_df[self.seq_df["chain_id"] == chain]
+
+    def fasta(self):
+        """Return the fasta dictionary
+
+        Returns
+        -------
+        fasta_dict : dict
+            A dictionary containing all the (author) chains in a fasta file (keys)
+            and their corresponding sequence (values)
+
+        """
+        return self.fasta_dict
+
+    def chains(self):
+        """Return the chains in the PDB
+
+        Returns
+        -------
+        chains : list
+            A list of chain identifiers
+
+        """
+        return list(self.fasta_dict.keys())
+
+    @lru_cache()
+    def _pdb_sequence(self, chain):
+        """Return the PDB sequence for a given chain ID"""
+        return "".join(self.sequence_df(chain)["residue_name"])
+
+    @lru_cache()
+    def _align(self, chain):
+        """Align the PDB sequence to the FASTA sequence for a given chain ID"""
+        chain_crd = self.pdb_df(chain)
+        pdb_seq = self._pdb_sequence(chain)
+        # aligner = PairwiseAligner()
+        # aligner.match_score = 2
+        # aligner.mismatch_score = -10
+        # aligner.open_gap_score = -0.5
+        # aligner.extend_gap_score = -0.1
+        # aligned_seq, fasta_seq = aligner.align(pdb_seq, fasta[chain])[0]
+        aligned_seq, fasta_seq, *_ = pairwise2.align.globalms(
+            pdb_seq, self.fasta_dict[chain], 2, -10, -0.5, -0.1
+        )[0]
+        if "-" in fasta_seq or "".join([x for x in aligned_seq if x != "-"]) != pdb_seq:
+            raise PDBError("Incorrect alignment")
+        return aligned_seq, fasta_seq
+
+    def alignment(self, chains=None):
+        """Return the alignment between the PDB and the FASTA sequence
+
+        Parameters
+        ----------
+        chains : list, optional
+            A list of chain identifiers (if not provided, all chains are aligned)
+
+        Returns
+        -------
+        alignment : dict
+            A dictionary containing the aligned sequences for each chain
+
+        """
+        if chains is None:
+            chains = self.chains()
+        return {chain: self._align(chain) for chain in chains}
+
+
+class SAbDabEntry(PDBEntry):
+    def __init__(
+        self,
+        pdb_path,
+        fasta_path,
+        heavy_chain=None,
+        light_chain=None,
+        antigen_chains=None,
+    ):
+        """
+        Parameters
+        ----------
+        pdb_path : str
+            Path to the PDB file
+        fasta_path : str
+            Path to the FASTA file
+        heavy_chain : str, optional
+            Heavy chain identifier (author chain name)
+        light_chain : str, optional
+            Light chain identifier (author chain name)
+        antigen_chains : list, optional
+            List of antigen chain identifiers (author chain names)
+
+        """
+        if heavy_chain is None and light_chain is None:
+            raise PDBError("At least one chain must be provided")
+        self.chain_dict = {
+            "heavy": heavy_chain,
+            "light": light_chain,
+        }
+        if antigen_chains is None:
+            antigen_chains = []
+        self.chain_dict["antigen"] = antigen_chains
+        super().__init__(pdb_path, fasta_path)
+
+    @staticmethod
+    def from_id(
+        pdb_id,
+        local_folder=".",
+        light_chain=None,
+        heavy_chain=None,
+        antigen_chains=None,
+    ):
+        pdb_path = download_pdb(pdb_id, local_folder)
+        fasta_path = download_fasta(pdb_id, local_folder)
+        return SAbDabEntry(
+            pdb_path=pdb_path,
+            fasta_path=fasta_path,
+            light_chain=light_chain,
+            heavy_chain=heavy_chain,
+            antigen_chains=antigen_chains,
+        )
+
+    def heavy_chain(self):
+        """Return the heavy chain identifier
+
+        Returns
+        -------
+        chain : str
+            The heavy chain identifier
+
+        """
+        return self.chain_dict["heavy"]
+
+    def light_chain(self):
+        """Return the light chain identifier
+
+        Returns
+        -------
+        chain : str
+            The light chain identifier
+
+        """
+        return self.chain_dict["light"]
+
+    def antigen_chains(self):
+        """Return the antigen chain identifiers
+
+        Returns
+        -------
+        chains : list
+            The antigen chain identifiers
+
+        """
+        return self.chain_dict["antigen"]
+
+    def chains(self):
+        """Return the chains in the PDB
+
+        Returns
+        -------
+        chains : list
+            A list of chain identifiers
+
+        """
+        return [self.heavy_chain(), self.light_chain()] + self.antigen_chains()
+
+    def chain_type(self, chain):
+        """Return the type of a chain
+
+        Parameters
+        ----------
+        chain : str
+            Chain identifier
+
+        Returns
+        -------
+        chain_type : str
+            The type of the chain (heavy, light or antigen)
+
+        """
+        for k, v in self.chain_dict.items():
+            if chain in v:
+                return k
+        raise PDBError("Chain not found")
+
+    @lru_cache()
+    def _get_cdr(self, chain, align_to_fasta=False):
+        chain_crd = self.pdb_df(chain)
+        chain_type = self.chain_type(chain)[0].upper()
+        if chain_type not in ["H", "L"]:
+            return None
+        pdb_seq = self._pdb_sequence(chain)
+        if "insertion" in chain_crd.columns:
+            chain_crd["residue_number"] = chain_crd.apply(
+                lambda row: f"{row['residue_number']}_{row['insertion']}", axis=1
+            )
+        unique_numbers = chain_crd["residue_number"].unique()
+        if len(unique_numbers) != len(pdb_seq):
+            raise PDBError("Inconsistencies in the biopandas dataframe")
+        cdr_arr = [CDR_VALUES[chain_type][int(x.split("_")[0])] for x in unique_numbers]
+        cdr_arr = np.array(cdr_arr)
+        if align_to_fasta:
+            aligned_seq, fasta_seq = self._align(chain)
+            aligned_seq_arr = np.array(list(aligned_seq))
+        cdr_arr[aligned_seq_arr != "-"] = _get_chothia_cdr(unique_numbers, chain_type)
+
+    def cdr(self):
+        if "insertion" in chain_crd.columns:
+            chain_crd["residue_number"] = chain_crd.apply(
+                lambda row: f"{row['residue_number']}_{row['insertion']}", axis=1
+            )
+        unique_numbers = chain_crd["residue_number"].unique()
+        if len(unique_numbers) != len(pdb_seq):
+            raise PDBError("Inconsistencies in the biopandas dataframe")
+        arr = [CDR_VALUES[chain_type][int(x.split("_")[0])] for x in num_array]
+        return np.array(arr)
--- proteinflow/data/utils.py
+++ proteinflow/data/utils.py
@@ -6,7 +6,14 @@ This module contains a function to annotate secondary structure elements (SSEs) 
 Adapted from: https://github.com/biotite-dev/biotite.
 """
 
+from copy import deepcopy
+
+from biopandas.mmcif import PandasMmcif
 from biotite.structure.geometry import angle, dihedral, distance
+
+
+class PDBError(ValueError):
+    pass
 
 
 def _annotate_sse(X):
@@ -164,3 +171,80 @@ def _dihedral_angle(crd, msk):
     dh = np.degrees(np.arctan2(y, x))
     dh[1 - msk] = 0
     return dh
+
+
+class CustomMmcif(PandasMmcif):
+    """
+    A modification of `PandasMmcif`
+
+    Adds a `get_model` method and renames the columns to match PDB format.
+    """
+
+    def read_mmcif(self, path: str):
+        x = super().read_mmcif(path)
+        x.df["ATOM"].rename(
+            {
+                "label_comp_id": "residue_name",
+                "label_seq_id": "residue_number",
+                "label_atom_id": "atom_name",
+                "group_PDB": "record_name",
+                "Cartn_x": "x_coord",
+                "Cartn_y": "y_coord",
+                "Cartn_z": "z_coord",
+            },
+            axis=1,
+            inplace=True,
+        )
+        x.df["ATOM"]["chain_id"] = x.df["ATOM"]["auth_asym_id"]
+        return x
+
+    def amino3to1(self):
+        df = super().amino3to1()
+        df.columns = ["chain_id", "residue_name"]
+        return df
+
+    def get_model(self, model_index: int):
+        """Returns a new PandasPDB object with the dataframes subset to the given model index.
+
+        Parameters
+        ----------
+        model_index : int
+            An integer representing the model index to subset to.
+
+        Returns
+        ---------
+        pandas_pdb.PandasPdb : A new PandasPdb object containing the
+          structure subsetted to the given model.
+        """
+
+        df = deepcopy(self)
+
+        if "ATOM" in df.df.keys():
+            df.df["ATOM"] = df.df["ATOM"].loc[
+                df.df["ATOM"]["pdbx_PDB_model_num"] == model_index
+            ]
+        return df
+
+
+def _retrieve_author_chain(chain):
+    """
+    Retrieve the (author) chain names present in the chain section (delimited by '|' chars) of a header line in a fasta file
+    """
+
+    if "auth" in chain:
+        return chain.split(" ")[-1][:-1]
+
+    return chain
+
+
+def _retrieve_chain_names(entry):
+    """
+    Retrieve the (author) chain names present in one header line of a fasta file (line that begins with '>')
+    """
+
+    entry = entry.split("|")[1]
+
+    if "Chains" in entry:
+        return [_retrieve_author_chain(e) for e in entry[7:].split(", ")]
+
+    return [_retrieve_author_chain(entry[6:])]
