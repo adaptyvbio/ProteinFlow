9b30e4e71213e8b1db9008db64d3464864b0921f
Liza Kozlova <liza@adaptyvbio.com>
2023-10-12 14:21:15 +0000

    chore: add a script to install all optional dependencies

---
+++ install_optional.sh
@@ -0,0 +1,12 @@
+python -m pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
+# choose the CUDA version that matches your system (check with `nvcc --version` and get the right command at https://pytorch.org/get-started/previous-versions/)
+
+python -m pip install "fair-esm[esmfold]"
+python -m pip install 'dllogger @ git+https://github.com/NVIDIA/dllogger.git'
+python -m pip install 'openfold @ git+https://github.com/aqlaboratory/openfold.git@4b41059694619831a7db195b7e0988fc4ff3a307'
+
+python -m pip install ablang
+python -m pip install igfold
+
+python -m pip install -e .
+python -m pip install ipykernel
